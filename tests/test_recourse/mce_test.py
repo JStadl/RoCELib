d6f1303ce948d7ce5c7eb2e696bdc52b0da3969f
Lorenzo <le522@ic.ac.uk>
2025-01-21 11:16:47 +0000

    [le522] Feat: changed tests to be less brittle by requiring a certain percentage of valid cfs

--- tests/test_recourse/mce_test.py
+++ tests/test_recourse/mce_test.py
@@ -2,6 +2,7 @@ from rocelib.datasets.ExampleDatasets import get_example_dataset
 from rocelib.models.pytorch_models.SimpleNNModel import SimpleNNModel
 from rocelib.recourse_methods.MCE import MCE
 from rocelib.tasks.ClassificationTask import ClassificationTask
+from ..test_constants import TEST_INSTANCES, PASS_THRESHOLD
 
 
 def test_mce_predicts_positive_instances():
@@ -14,14 +15,16 @@ def test_mce_predicts_positive_instances():
     ct.train()
 
     recourse = MCE(ct)
+    counterfactuals = 0
 
-    # _, neg = list(dl.get_negative_instances(neg_value=0).iterrows())[0]
-
-    for _, neg in dl.get_negative_instances(neg_value=0).iterrows():
+    for _, neg in dl.get_negative_instances(neg_value=0).head(TEST_INSTANCES).iterrows():
 
         res = recourse.generate_for_instance(neg)
 
         if not res.empty:
             prediction = model.predict_single(res)
 
-            assert prediction
+            if prediction:
+                counterfactuals += 1
+
+    assert counterfactuals >= TEST_INSTANCES * PASS_THRESHOLD
--- tests/test_recourse/rnce_test.py
+++ tests/test_recourse/rnce_test.py
@@ -2,6 +2,7 @@ from rocelib.datasets.ExampleDatasets import get_example_dataset
 from rocelib.models.pytorch_models.SimpleNNModel import SimpleNNModel
 from rocelib.recourse_methods.RNCE import RNCE
 from rocelib.tasks.ClassificationTask import ClassificationTask
+from ..test_constants import TEST_INSTANCES, PASS_THRESHOLD
 
 
 def test_rnce() -> None:
@@ -16,9 +17,11 @@ def test_rnce() -> None:
     delta = 0.01
 
     recourse = RNCE(ct)
+    counterfactuals = 0
 
-    _, neg = list(dl.get_negative_instances(neg_value=0).iterrows())[0]
-
-    for _, neg in dl.get_negative_instances(neg_value=0).head(10).iterrows():
+    for _, neg in dl.get_negative_instances(neg_value=0).head(TEST_INSTANCES).iterrows():
         res = recourse.generate_for_instance(neg, delta=delta)
-        assert recourse.intabs.evaluate(res, delta=delta)
+        if recourse.intabs.evaluate(res, delta=delta):
+            counterfactuals += 1
+
+    assert counterfactuals >= TEST_INSTANCES * PASS_THRESHOLD
