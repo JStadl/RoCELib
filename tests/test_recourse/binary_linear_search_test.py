1f93fa5007196e0d09208946168ecd834507dd94
Lorenzo <le522@ic.ac.uk>
2025-01-24 12:25:56 +0000

    [le522] Fix: removed local path added for testing that was committed

--- tests/test_recourse/binary_linear_search_test.py
+++ tests/test_recourse/binary_linear_search_test.py
@@ -14,7 +14,7 @@ from rocelib.tasks.ClassificationTask import ClassificationTask
 def test_binary_linear_search_nn() -> None:
     # Create a new classification task and train the model on our data
     model = SimpleNNModel(10, [7], 1)
-    dl = CsvDatasetLoader('/Users/lorenzo/Documents/imperial/RoCELib/tests/assets/recruitment_data.csv', "HiringDecision")
+    dl = CsvDatasetLoader('./assets/recruitment_data.csv', "HiringDecision")
     ct = ClassificationTask(model, dl)
 
     ct.train()
